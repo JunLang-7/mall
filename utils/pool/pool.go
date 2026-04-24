5bb594462d2f6e6095afa75e1974c8793b6de1d7
Junlang-7 <1448760937@qq.com>
2026-04-24 20:47:24 +0800

    fix(pool): 修复协程池中协程提前退出问题，实现支持任务执行的等待和管理

--- utils/pool/pool.go
+++ utils/pool/pool.go
@@ -1,24 +1,36 @@
 package pool
 
 import (
+	"sync"
+
 	"github.com/panjf2000/ants/v2"
 )
 
 type Pool struct {
 	pool *ants.Pool
+	wg   *sync.WaitGroup
 }
 
 func NewPoolWithSize(size int) *Pool {
 	pool, _ := ants.NewPool(size)
 	return &Pool{
 		pool: pool,
+		wg:   &sync.WaitGroup{},
 	}
 }
 
 func (p *Pool) RunGo(f func()) {
-	_ = p.pool.Submit(f)
+	p.wg.Add(1)
+	_ = p.pool.Submit(func() {
+		defer p.wg.Done()
+		f()
+	})
 }
 
 func (p *Pool) Wait() {
+	p.wg.Wait()
+}
+
+func (p *Pool) Release() {
 	p.pool.Release()
 }
